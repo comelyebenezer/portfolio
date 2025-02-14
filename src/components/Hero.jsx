edadb0475723d534b083104158265be691e599b6
unknown <comely.jr@gmail.com>
2025-02-14 14:27:17 -0800

    second commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,22 @@
 import React from 'react'
 import comelyprofile from "../assets/comelyprofile.webp"
 import { HERO_CONTENT } from "../constants"
+import { motion } from 'framer-motion'
+
+
+
+const containerVaraints = {
+    hidden: { opacity: 0, x: -100 },
+    visible: { opacity: 1, x: 0, transition: { duration: 0.5, staggerChildren: 0.5, }}
+}
+
+const childVaraints = {
+    hidden: { opacity: 0, x: -100 },
+    visible: { opacity: 1, x: 0, transition: { duration: 0.5, }}
+}
+
+
+
 
 const Hero = () => {
   return (
@@ -8,19 +24,41 @@ const Hero = () => {
         <div className='flex flex-wrap lg:flex-row-reverse'>
             <div className='w-full lg:w-1/2'>
                 <div className='flex justify-center lg:p-8'>
-                    <img src={comelyprofile} alt="comely profile picture" className='border border-stone-900
-                    rounded-3xl' />
+                    <motion.img src={comelyprofile} alt="comely profile picture" className='border border-stone-900
+                    rounded-3xl'
+                    width={650}
+                    height={650}
+                    initial={{ x:100, opacity: 0 }} 
+                    animate={{ x:0, opacity:1 }}
+                    transition={{ transition:1, delay: 1.5 }}
+                    />
                 </div>
             </div>
             <div className='w-full lg:w-1/2'>
-                <div className='flex flex-col items-center lg:items-start mt-10'>
-                    <h2 className='pb-2 text-4xl tracking-tighter lg:text-8xl'>Comely Ebenezer</h2>
-                    <span className='bg-gradient-to-r from-stone-300 to-stone-600
-                    bg-clip-text text-3xl tracking-tight text-transparent'> Front End Developer</span>
-                    <p className='my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter'> {HERO_CONTENT}</p>
-                    <a href="/resume.pdf" target='blank' rel='noopener noreferrer' download 
-                    className='bg-white rounded-md p-4 text-sm text-stone-800 mb-10'>Download Resume</a>
-                </div>
+                <motion.div
+                    initial="hidden"
+                    animate='visible'
+                    variants={ containerVaraints } 
+                    className='flex flex-col items-center lg:items-start mt-10'>
+
+                        <motion.h2 
+                        variants={childVaraints}
+                        className='pb-2 text-4xl tracking-tighter lg:text-8xl'>Comely Ebenezer</motion.h2>
+
+                        <motion.span 
+                        variants={childVaraints} 
+                            className='bg-gradient-to-r from-stone-300 to-stone-600
+                            bg-clip-text text-2xl tracking-tight text-transparent lg:3xl'> Front End Developer</motion.span>
+
+                            <motion.p 
+                            variants={childVaraints} 
+                                className='my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter'> {HERO_CONTENT}</motion.p>
+
+                            <motion.a 
+                            variants={childVaraints} 
+                                href="/resume.pdf" target='blank' rel='noopener noreferrer' download 
+                                className='bg-white rounded-md p-4 text-sm text-stone-800 mb-10'>Download Resume</motion.a>
+                </motion.div>
             </div>
         </div>
     </div>
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -6,42 +6,93 @@ import { FaHtml5 } from "react-icons/fa";
 import { FaCss3 } from "react-icons/fa";
 import { TbBrandTailwind } from "react-icons/tb";
 import { TbBrandJavascript } from "react-icons/tb";
+import { motion } from 'framer-motion';
 
 
+const iconVariants = (duration) => ({
+    initial: { y: -10 },
+    animate: { 
+        y: [10, -10],
+        transition: { 
+            duration: duration,
+            ease: "linear",
+            repeat: Infinity,
+            repeatType: "reverse",
+         }
 
-
-
+     },
+})
 
 const Technologies = () => {
   return (
     <div className='pb-24'>
-        <h2 className='my-20 text-center text-4xl'>Technologies</h2>
-        <div className='flex flex-wrap items-center justify-center gap-4'>
-            <div>
-                <RiReactjsLine className='text-7xl text-cyan-400' />
-            </div>
-            <div className='p-4'>
-                <TbBrandJavascript  className='text-7xl text-yellow-600' />
-            </div>
-            <div className='p-4'>
-                <FaHtml5 className='text-7xl text-red-700' />
+        <motion.h2 
+            whileInView={{ opacity: 1, y: 0}}
+            initial={{ opacity:0, y:-100 }}
+            transition={{ duration: 1.5 }}
+            className='my-20 text-center text-4xl'>Technologies</motion.h2>
 
-            </div>
+        <motion.div
+            whileInView={{ opacity: 1, x: 0}}
+            initial={{ opacity:0, x:-100 }}
+            transition={{ duration: 1.5 }}
+             className='flex flex-wrap items-center justify-center gap-4'>
 
-            <div className='p-4'>
-                <FaCss3 className='text-7xl text-blue-600' />
-            </div>
+                <motion.div 
+                    initial = "initial"
+                    animate = "animate"
+                    variants = {iconVariants(2.5)}>
+                    <RiReactjsLine className='text-7xl text-cyan-400' />
+                </motion.div>
 
-            <div className='p-4'>
-                <TbBrandTailwind  className='text-7xl text-blue-500' />
-            </div>
-            <div className='p-4'>
-                <FaNodeJs className='text-7xl text-green-500' />
-            </div>
-            <div className='p-4'>
-                <BiLogoPostgresql className='text-7xl text-sky-700' />
-            </div>
-        </div>
+                <motion.div 
+                    initial = "initial"
+                    animate = "animate"
+                    variants = {iconVariants(3)}
+                    className='p-4'>
+                    <TbBrandJavascript  className='text-7xl text-yellow-600' />
+                </motion.div>
+
+                <motion.div 
+                    initial = "initial"
+                    animate = "animate"
+                    variants = {iconVariants(5)} 
+                     className='p-4'>
+                    <FaHtml5 className='text-7xl text-red-700' />
+                </motion.div>
+
+                <motion.div 
+                    initial = "initial"
+                    animate = "animate"
+                    variants = {iconVariants(4)} 
+                    className='p-4'>
+                    <FaCss3 className='text-7xl text-blue-600' />
+                </motion.div>
+
+                <motion.div 
+                    initial = "initial"
+                    animate = "animate"
+                    variants = {iconVariants(6)} 
+                    className='p-4'>
+                    <TbBrandTailwind  className='text-7xl text-blue-500' />
+                </motion.div>
+
+                <motion.div 
+                    initial = "initial"
+                    animate = "animate"
+                    variants = {iconVariants(2)} 
+                    className='p-4'>
+                    <FaNodeJs className='text-7xl text-green-500' />
+                </motion.div>
+
+                <motion.div 
+                    initial = "initial"
+                    animate = "animate"
+                    variants = {iconVariants(7)} 
+                    className='p-4'>
+                    <BiLogoPostgresql className='text-7xl text-sky-700' />
+                </motion.div>
+        </motion.div>
     </div>
   )
 }
